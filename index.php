8d079a0cadd5bf600d1f4ec2712136d615fc9a2c
Flávio Silveira <flavioaugustosilveira@gmail.com>
2020-01-01 17:46:49 -0300

    The Dependency Injection Container

--- index.php
+++ index.php
@@ -1,7 +1,23 @@
 <?php
 declare(strict_types=1);
 
+use DI\ContainerBuilder;
+use App\HelloWorld;
+use function DI\create;
+
+// Autoload
 require_once './vendor/autoload.php';
 
-$helloWorld = new \App\HelloWorld();
+// DI-Container
+$containerBuilder = new ContainerBuilder();
+$containerBuilder->useAutowiring(false);
+$containerBuilder->useAnnotations(false);
+$containerBuilder->addDefinitions([
+    HelloWorld::class => create(HelloWorld::class)
+]);
+
+$container = $containerBuilder->build();
+
+// App
+$helloWorld = $container->get(HelloWorld::class);
 $helloWorld->announce();
